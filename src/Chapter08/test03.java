6b18fac11d572f29da5af672d7623977b028c5c7
lbaitian <lbaitian6663@gmail.com>
2023-12-07 23:01:00 +0900

    常用API的ArrayList

---
+++ src/Chapter08/test03.java
@@ -0,0 +1,12 @@
+package Chapter08;
+
+public class test03 {
+    //需求：以及菜品信息浏览功能
+    //目标：使用所学的ArrayList集合结合面对对象编程实现以上两个需求
+    public static void main(String[] args) {
+    //1.设计菜品类report03,负责创建菜品对象，封装菜品数据
+    //2.设计一个菜品操作类reportOperator03，负责完成对菜品业务实现
+        reportOperator03 operator = new reportOperator03();
+        operator.start();
+    }
+}
